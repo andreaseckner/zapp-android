d06dcc1048c26cddbf15d8bc5f628755dc4fb35e
Andreas Eckner <andreas.eckner@gmail.com>
2019-04-08 09:14:36 +0200

    add exercise for 1.01 - Activities and Layouts - Hello World

--- app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
+++ app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
@@ -4,10 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // TODO (3) Create a TAG String constant TAG to store the name of the class
+
+    // TODO (4) Declare a property for the button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO (5) Assign the button from the layout to the class property
+
+        // TODO (6) Create an anonymous OnClickListener and assign it to the button
+        // TODO (7) Log a message with the given TAG property and String "Hello World"
     }
 }
