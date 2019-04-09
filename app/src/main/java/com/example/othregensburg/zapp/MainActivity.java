3de03f13b0afb59cf48ae8488417d7aac29fb3f8
Andreas Eckner <andreas.eckner@gmail.com>
2019-04-09 08:39:39 +0200

    add exercise for 1.02 - Activities and Layouts - Intents

--- app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
+++ app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
 
+    // TODO (3) Create a new Activity "SecondActivity" in the same package, (Hint: File -> New -> Activity -> Empty Activity)
+    // TODO (4) Change the layout for the "SecondActivity" to show a TextView in the middle of the screen
+    // TODO (5) Add a new property for the EditText field and bind the view from the layout (Hint: use findViewById in the onCreate method)
+
     Button mHelloWorldButton;
 
     @Override
@@ -25,4 +29,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // TODO (6) Add a method for the previously created button in the layout file
+    // TODO (7) Get the value from the EditText field
+    // TODO (8) Create an Intent to open the SecondActivity
+    // TODO (9) Put the String from the EditText to the Intent (Hint: Intent#putExtra(name, value))
+    // TODO (10) Start the SecondActivity
+    // TODO (11) In the SecondActivity read the data from the Intent and show the String in the TextView
+
 }
