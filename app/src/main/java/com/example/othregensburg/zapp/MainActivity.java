aa0917850e94a0a64012be4826315bf25ae061e3
Andreas Eckner <andreas.eckner@gmail.com>
2019-04-15 01:37:10 +0200

    add exercise for 2.01 - Recycler View - Basics

--- app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
+++ app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.othregensburg.zapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final LinkedList<String> mList = new LinkedList<>();
+
+    // TODO (4) Create a new class for the adapter which extends from RecyclerView.Adapter<CustomAdapter.ViewHolder>
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random rnd = new Random();
+        for (int i = 0; i < 50; i++)
+            mList.add(((char) ('A' + rnd.nextInt('Z' - 'A'))) + " " + Integer.toString(i));
+        Collections.sort(mList);
+
+        // TODO (5) Create member variables and instantiate the Recycler View and the Adapter
+
+        // TODO (6) Set the new Adapter to the Recycler View
+
+        // TODO (7) Set a LayoutManager to the Recycler View
     }
 }
