14ddba6a4a015b829edca36b73d0b7f9acd9ca96
Andreas Eckner <andreas.eckner@gmail.com>
2019-04-29 23:05:22 +0200

    add exercise for 3.03 - Material Design - ActionBar Toggle

--- app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
+++ app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.othregensburg.zapp;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +10,9 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +21,24 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
+    // TODO(2) Add onPostCreate method to sync the drawer toggle state
+
     private void init() {
         setupUiComponents();
         setupToolbar();
+        setupDrawerToggle();
     }
 
     private void setupUiComponents() {
         mToolbar = findViewById(R.id.main_toolbar);
+        mDrawerLayout = findViewById(R.id.main_drawer_layout);
     }
 
     private void setupToolbar() {
         setSupportActionBar(mToolbar);
+    }
+
+    private void setupDrawerToggle() {
+        // TODO (1) Add ActionBarDrawerToggle and assign it to the drawer layout listener
     }
 }
