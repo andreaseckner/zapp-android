29c221da19b4aad838e3870c4eee1b00ead5145f
Andreas Eckner <andreas.eckner@gmail.com>
2019-04-29 21:37:18 +0200

    add exercise for 3.01 - Material Design - Drawer Layout

--- app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
+++ app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.othregensburg.zapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        setupUiComponents();
+        setupToolbar();
+    }
+
+    private void setupUiComponents() {
+        // TODO (5) Get a reference of the toolbar
+    }
+
+    private void setupToolbar() {
+        // TODO (6) Set toolbar as ActionBar (setSupportActionBar)
     }
 }
