ac7b90a24f6353c89642e5dbbc848205617e5e3f
Andreas Eckner <andreas.eckner@gmail.com>
2019-05-26 21:47:34 +0200

    add solution for 4.01 - Persistence - SharedPreferences

--- app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
+++ app/src/main/java/com/example/othregensburg/zapp/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.othregensburg.zapp;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String COUNTER = "counter";
+
+    private TextView mTextView;
+    private Integer mCounter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (2) Get default SharedPreferences via PreferenceManager
-        // TODO (3) Load counter preference and update the text view
-        // TODO (4) Increase counter value and save it to SharedPreferences
+        mTextView = findViewById(R.id.counterTextView);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mCounter = prefs.getInt(COUNTER, 1);
+        mTextView.setText(String.format(Locale.GERMAN, "%d", mCounter));
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(COUNTER, ++mCounter);
+        editor.apply();
+
     }
 }
